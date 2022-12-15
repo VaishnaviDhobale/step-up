c1c1aaf2d14fdcd0dd46cb0d053817ad98b34bc4
Nikhil <inikhilmalik@gmail.com>
2022-12-15 23:25:27 +0530

    day3

---
+++ scripts/preference.js
@@ -0,0 +1,225 @@
+let btnlist = document.querySelectorAll("#btnlist li");
+let secondS = document.getElementById("secondS");
+start();
+function start()
+{
+    secondS.innerHTML=`
+    <div id="profile">
+                    <div id="p1">
+                        <h3>Profile Settings</h3>
+                        <button>Save</button>
+                    </div>
+                    <label>Upload Profile</label>
+                    <div id="p2">
+                        <div>
+                            <img width="100px" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="pic">
+                        </div>
+                        <p>Only JPG, PNG and JPEG files are supported.</p>
+                    </div>
+                    <div id="p3">
+                        <div>
+                            <label>Name</label>
+                            <div>
+                                <input type="text" placeholder="Name">
+                            </div>
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <div>
+                                <input  placeholder="yourEmailid@gmail.com">
+                            </div>
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <div>
+                                <input type="text" placeholder="Password">
+                            </div>
+                        </div>
+                        <div>
+                            <label>Role</label>
+                            <div>
+                                <select name="" id=""></select>
+                            </div>
+                        </div>
+                        <div>
+                            <label>Phone Number</label>
+                            <div>
+                                <input type="text" placeholder="Phone Number">
+                            </div>
+                        </div>
+                        <div>
+                            <label>Curruancy</label>
+                            <div>
+                                <select name="" id="">
+                                    <option>Set an Option</option>
+                                    <option>United States Dollar</option>
+                                    <option>Inidan Rupee</option>
+                                    <option>Indonesian Rupiah</option>
+                                    <option>Euro</option>
+                                </select>
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+    `
+}
+btnlist[0].addEventListener("click", function () {
+    secondS.innerHTML=`
+    <div id="profile">
+                    <div id="p1">
+                        <h3>Profile Settings</h3>
+                        <button>Save</button>
+                    </div>
+                    <label>Upload Profile</label>
+                    <div id="p2">
+                        <div>
+                            <img width="100px" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="pic">
+                        </div>
+                        <p>Only JPG, PNG and JPEG files are supported.</p>
+                    </div>
+                    <div id="p3">
+                        <div>
+                            <label>Name</label>
+                            <div>
+                                <input type="text" placeholder="Name">
+                            </div>
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <div>
+                                <input  placeholder="yourEmailid@gmail.com">
+                            </div>
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <div>
+                                <input type="text" placeholder="Password">
+                            </div>
+                        </div>
+                        <div>
+                            <label>Role</label>
+                            <div>
+                                <select name="" id=""></select>
+                            </div>
+                        </div>
+                        <div>
+                            <label>Phone Number</label>
+                            <div>
+                                <input type="text" placeholder="Phone Number">
+                            </div>
+                        </div>
+                        <div>
+                            <label>Curruancy</label>
+                            <div>
+                                <select name="" id="">
+                                    <option>Set an Option</option>
+                                    <option>United States Dollar</option>
+                                    <option>Inidan Rupee</option>
+                                    <option>Indonesian Rupiah</option>
+                                    <option>Euro</option>
+                                </select>
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+    `
+})
+
+btnlist[4].addEventListener("click", function () {
+secondS.innerHTML = `
+<div id="sync">
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>
+<div>
+<img width="35%" src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/img/google-contacts.png">
+<p>Sync all your contacts, companies, tracks and deals from Google to EngageBay.</p>
+<button>Enable</button>
+</div>   
+</div>
+`;
+})
